5521ee8bd6e5d9a7b5fce40be476732219f19371
Daniel <danielarta1992@gmail.com>
2025-01-06 01:44:05 -0500

    Fron-End Se actualiza el menu de las paginas de usuarios no logeados para que correspond con las opciones permitidas para este estado de usuario

--- public/menu_out.php
+++ public/menu_out.php
@@ -5,33 +5,6 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="dashboard.php">Inicio</a>
-            </li> -->
-            <!-- <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Tienda
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="tienda.php">Ver Tienda</a>
-                    <a class="dropdown-item" href="#">Crear Productos</a>
-                </div>
-            </li> -->
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="cart.php">Carrito</a>
-            </li> -->
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="ver_facturas.php">Facturas</a>
-            </li> -->
-            <!-- <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Perfil
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="mostrar_perfil.php">Ver Perfil</a>
-                    <a class="dropdown-item" href="perfil.php">Editar Perfil</a>
-                </div>
-            </li> -->
             <li class="nav-item">
                 <a class="btn btn-warning" href="index.php">Crear Cuenta</a>
                 <a class="btn btn-warning" href="login.php">Iniciar Sesión</a>
